10337d2e4a0599534113906508b5bc57252f4d95
Lauri Kasanen <cand@gmx.com>
2023-04-05 18:02:16 +0300

    Asm-optimize 2bit, -9.5%

--- tgt/decomp.c
+++ tgt/decomp.c
@@ -46,27 +46,7 @@ u8 decomp_flat(const u8 *in, u8 *out);
 
 u8 decomp_1bit(const u8 *in, u8 *out);
 
-static u8 decomp_2bit(const u8 *in, u8 *out) {
-	u16 used, val;
-	u8 tab[4], i, cur = 0, j;
-	memcpy(&used, in, 2);
-	in += 2;
-
-	for (i = 0; i < 16; i++) {
-		if (used & (1 << i))
-			tab[cur++] = i;
-	}
-	for (i = 0; i < 16; i += 2) {
-		memcpy(&val, in, 2);
-		in += 2;
-		for (j = 0; j < 16; j += 2) {
-			*out++ = tab[val & 3];
-			val >>= 2;
-		}
-	}
-
-	return 18;
-}
+u8 decomp_2bit(const u8 *in, u8 *out);
 
 static u8 decomp_3bit(const u8 *in, u8 *out) {
 	u8 tab[8], i, cur = 0, j;
